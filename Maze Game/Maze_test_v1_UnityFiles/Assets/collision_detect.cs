d3f2792b7238f7d73507aad0b4d21fa17e54a3d6
Beth Seethaler <beth.seethaler@gmail.com>
2015-10-08 20:08:15 -0600

    made a character controller and colliders for them.

---
+++ Maze Game/Maze_test_v1_UnityFiles/Assets/collision_detect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision_detect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
